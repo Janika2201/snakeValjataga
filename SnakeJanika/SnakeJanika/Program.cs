6622568353457fb8086bab19fd532fefcbe50c38
janika <zajcevsmajlik@gmail.com>
2020-03-25 13:23:34 +0200

    rabota

---
+++ SnakeJanika/SnakeJanika/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeJanika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
